92e0d8d1de941fe5e2d5257884e0641cae9e04ef
Md-Kutub-Islam <mdkutubislam09@gmail.com>
2024-09-21 12:06:56 +0530

    update bcryptjs

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,115 +1,115 @@
-import AppError from '../utills/errorUtills.js'
-import User from '../model/user.js'
-import bcrypt from 'bcrypt'
+import AppError from "../utills/errorUtills.js";
+import User from "../model/user.js";
+import bcrypt from "bcryptjs"; // Instead of 'bcrypt'
 
-
-// cookieOption 
+// cookieOption
 const cookieOption = {
-    maxAge:  1 * 24 * 60 * 1000, // 7 days
-    httpOnly: true,
-    // secure: true
-}
-if(process.env.NODE_ENV === 'production'){
-    cookieOption.secure = true
+  maxAge: 1 * 24 * 60 * 1000, // 7 days
+  httpOnly: true,
+  // secure: true
+};
+if (process.env.NODE_ENV === "production") {
+  cookieOption.secure = true;
 }
 
-// Register user 
+// Register user
 export const register = async (req, res, next) => {
-    try {
-        // const { firstName, lastName, email, password, picturePath, friends, skills, stream, bio } = req.body
-        
-        const {
-            firstName,
-            lastName,
-            email,
-            password,
-            picturePath,
-            friends,
-            stream,
-            skills,
-            achievement,
-            bio
-        } = req.body;
-      
-        const salt = await bcrypt.genSalt();
-        const passwordHash = await bcrypt.hash(password, salt);
-           
-        if(!firstName || !lastName || !email || !password ){
-            return next(new AppError('All field are required', 400))
-        }
+  try {
+    // const { firstName, lastName, email, password, picturePath, friends, skills, stream, bio } = req.body
 
-        // Check user is exists or not already 
-        const userExists = await User.findOne({email})
-        if(userExists){
-            return next (new AppError('Email is already exists', 400))
-        }
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      stream,
+      skills,
+      achievement,
+      bio,
+    } = req.body;
 
-        const user = await User.create({
-            firstName,
-            lastName,
-            email,
-            password:  passwordHash,
-            picturePath,
-            friends,
-            stream,
-            skills,
-            achievement,
-            bio,
-            viewedProfile: Math.floor(Math.random() * 10000),
-            impressions: Math.floor(Math.random() * 10000),
-        })
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
 
-        if(!user){
-            return next (new AppError('User resgistration faield, please try again', 400))
-        }
-
-        await user.save()
-
-        user.password = undefined // we dont want to send password to end user that why we undefined password
-
-        // before we go to registerd we have to generate jwt token which store inside the cookie 
-        const token = await user.generateJWTToken()
-        res.cookie('token', token, cookieOption)
-
-        res.status(200).json({
-            success: true,
-            message: 'User registered successfully',
-            user
-        })
-    } catch (error) {
-        res.status(500).json({error: error.message})
+    if (!firstName || !lastName || !email || !password) {
+      return next(new AppError("All field are required", 400));
     }
-}
+
+    // Check user is exists or not already
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return next(new AppError("Email is already exists", 400));
+    }
+
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      stream,
+      skills,
+      achievement,
+      bio,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+
+    if (!user) {
+      return next(
+        new AppError("User resgistration faield, please try again", 400)
+      );
+    }
+
+    await user.save();
+
+    user.password = undefined; // we dont want to send password to end user that why we undefined password
+
+    // before we go to registerd we have to generate jwt token which store inside the cookie
+    const token = await user.generateJWTToken();
+    res.cookie("token", token, cookieOption);
+
+    res.status(200).json({
+      success: true,
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 export const login = async (req, res, next) => {
-    try {
-        const {email, password} = req.body
+  try {
+    const { email, password } = req.body;
 
-        if(!email || !password ){
-            return next(new AppError('All field are required', 400))
-        }
-
-        const user = await User.findOne({email})
-
-        if(!user){
-            return next(new AppError('Email or password does not match', 400))
-        }
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
-
-        const token = await user.generateJWTToken()
-        user.password = undefined
-
-        res.cookie('token', token, cookieOption)
-
-        res.status(200).json({
-            success: true,
-            message: 'User loggedin successfully',
-            token,
-            user
-        })
-    } catch (error) {
-        res.status(500).json({error: error.message})
-        
+    if (!email || !password) {
+      return next(new AppError("All field are required", 400));
     }
-}
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return next(new AppError("Email or password does not match", 400));
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
+
+    const token = await user.generateJWTToken();
+    user.password = undefined;
+
+    res.cookie("token", token, cookieOption);
+
+    res.status(200).json({
+      success: true,
+      message: "User loggedin successfully",
+      token,
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,128 +1,136 @@
 import User from "../model/user.js";
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"; // Instead of 'bcrypt'
 import AppError from "../utills/errorUtills.js";
 
-// Read 
+// Read
 export const getUser = async (req, res) => {
+  try {
     try {
-        try {
-            const { id } = req.params;
-            const user = await User.findById(id);
-            res.status(200).json({
-                success: true,
-                message: 'User details',
-                user
-            })
-          } catch (err) {
-            res.status(404).json({ message: err.message });
-          }
-    } catch (error) {
-        return next(new AppError('faild to fetch profile', 500))
+      const { id } = req.params;
+      const user = await User.findById(id);
+      res.status(200).json({
+        success: true,
+        message: "User details",
+        user,
+      });
+    } catch (err) {
+      res.status(404).json({ message: err.message });
     }
-}
+  } catch (error) {
+    return next(new AppError("faild to fetch profile", 500));
+  }
+};
 
 //Update user
 export const updateUser = async (req, res, next) => {
   try {
-    const {id} = req.params
-    const {firstName, lastName, stream, skills, achievement, twitter, linkdin, bio} = req.body
-    console.log("firstName:",firstName);
-    const user = await User.findById(id)
+    const { id } = req.params;
+    const {
+      firstName,
+      lastName,
+      stream,
+      skills,
+      achievement,
+      twitter,
+      linkdin,
+      bio,
+    } = req.body;
+    console.log("firstName:", firstName);
+    const user = await User.findById(id);
 
-    if(!user){
-      return next(new AppError('User does not exist', 400))
-    }
-
-    if(firstName){
-      user.firstName = firstName
-    }
-    if(lastName){
-      user.lastName = lastName
-    }
-    if(stream){
-      user.stream = stream
-    }
-    if(skills){
-      user.skills = skills
-    }
-    if(achievement){
-      user.achievement = achievement
-    }
-    if(twitter){
-      user.twitter = twitter
-    }
-    if(linkdin){
-      user.linkdin = linkdin
-    }
-    if(bio){
-      user.bio = bio
+    if (!user) {
+      return next(new AppError("User does not exist", 400));
     }
 
-    const updatedUser = await user.save()
+    if (firstName) {
+      user.firstName = firstName;
+    }
+    if (lastName) {
+      user.lastName = lastName;
+    }
+    if (stream) {
+      user.stream = stream;
+    }
+    if (skills) {
+      user.skills = skills;
+    }
+    if (achievement) {
+      user.achievement = achievement;
+    }
+    if (twitter) {
+      user.twitter = twitter;
+    }
+    if (linkdin) {
+      user.linkdin = linkdin;
+    }
+    if (bio) {
+      user.bio = bio;
+    }
+
+    const updatedUser = await user.save();
 
     res.status(200).json({
       success: true,
-      message: 'User details',
-      updatedUser
-    })
-
+      message: "User details",
+      updatedUser,
+    });
   } catch (error) {
-    new AppError(error || 'User not updated, please try again', 500)
+    new AppError(error || "User not updated, please try again", 500);
   }
-}
+};
 
 export const getUserFriend = async (req, res) => {
-    try {
-        const {id} =req.params
-        const user = await User.findById(id)
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
 
-        const friends = await Promise.all(
-            user.friends.map((id) => User.findById(id))
-        )
-        const formattedFriend = friends.map(
-            ({_id, firstName, lastName, stream, skills, bio, picturePath}) => {
-                return {_id, firstName, lastName, stream, skills, bio, picturePath}
-            }
-        )
-        res.status(200).json(formattedFriend)
-    } catch (error) {
-        res.status(404).json({message: error.message})
-    }
-}
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriend = friends.map(
+      ({ _id, firstName, lastName, stream, skills, bio, picturePath }) => {
+        return { _id, firstName, lastName, stream, skills, bio, picturePath };
+      }
+    );
+    res.status(200).json(formattedFriend);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 // Update delete and add friend
 export const addRemoveFriend = async (req, res) => {
-    try {
-      const { id, friendId } = req.params;
-      const user = await User.findById(id);
-      const friend = await User.findById(friendId);
+  try {
+    const { id, friendId } = req.params;
+    const user = await User.findById(id);
+    const friend = await User.findById(friendId);
 
-      if(!user && !friend){
-        console.log("user and friend not found");
+    if (!user && !friend) {
+      console.log("user and friend not found");
+    }
+
+    if (user.friends.includes(friendId)) {
+      user.friends = user.friends.filter((id) => id !== friendId);
+      friend.friends = friend.friends.filter((id) => id !== id);
+    } else {
+      user.friends.push(friendId);
+      friend.friends.push(id);
+    }
+    await user.save();
+    await friend.save();
+
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
       }
-  
-      if (user.friends.includes(friendId)) {
-        user.friends = user.friends.filter((id) => id !== friendId);
-        friend.friends = friend.friends.filter((id) => id !== id);
-      } else {
-        user.friends.push(friendId);
-        friend.friends.push(id);
-      }
-      await user.save();
-      await friend.save();
-  
-      const friends = await Promise.all(
-        user.friends.map((id) => User.findById(id))
-      );
-      const formattedFriends = friends.map(
-        ({ _id, firstName, lastName, occupation, location, picturePath }) => {
-          return { _id, firstName, lastName, occupation, location, picturePath };
-        }
-      );
-  
-      res.status(200).json(formattedFriends);
-    } catch (err) {
+    );
+
+    res.status(200).json(formattedFriends);
+  } catch (err) {
     //   res.status(404).json({ message: err.message });
     console.log(err);
-    }
-  };
+  }
+};
